68ce3479678eb32f8807e8ec5090ec810bc561b7
Hexy32 <sunshine.goody8762@gmail.com>
2023-09-21 17:49:04 -0400

    Re-did the styling, design, and UX.

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,53 +1,50 @@
-import checked_image from '../images/_checked_ symbol.png'
-import delete_image from '../images/_delete_ symbol.png'
+import checked_image from '../images/_checked_ symbol.svg'
+import delete_image from '../images/_delete_ symbol.svg'
 import styles from './styles/ItemList.module.css'
-import unchecked_image from '../images/_unchecked_ symbol.png'
+import unchecked_image from '../images/_unchecked_ symbol.svg'
 
 export default function ItemList({ items, setItems }: ItemListProps) {
-	return (
-		<div className={styles.container}>
-			{items.map((item, i) => (
-				<div
-					className={styles.itemContainer}
-					key={i}>
-					<button
-						onClick={() => {
-							const newArr = [...items]
-							newArr[i].checked = !item.checked
-							setItems(newArr)
-						}}
-						className={styles.checkBtn}>
-						<img
-							src={item.checked ? checked_image : unchecked_image}
-							alt=''
-						/>
-					</button>
-					<p className={styles.itemContent}>{item.content}</p>
-
-					<button
-						className={styles.deleteBtn}
-						onClick={() => {
-							const newArr = [...items]
-							newArr.splice(i, 1)
-							setItems(newArr)
-						}}>
-						<img
-							src={delete_image}
-							alt=''
-						/>
-					</button>
-				</div>
-			))}
-		</div>
-	)
+  return (
+    <div className={styles.container}>
+      {items.map((item, i) => (
+        <div className={styles.itemContainer} key={i}>
+          <p className={styles.itemContent}>{item.content}</p>
+          <div className={styles.controls}>
+            <button
+              className={styles.deleteBtn}
+              onClick={() => {
+                const newArr = [...items]
+                newArr.splice(i, 1)
+                setItems(newArr)
+              }}>
+              <img src={delete_image} alt="" />
+            </button>
+            <div className={styles.divider} />
+            <button
+              onClick={() => {
+                const newArr = [...items]
+                newArr[i].checked = !item.checked
+                setItems(newArr)
+              }}
+              className={styles.checkBtn}>
+              <img
+                src={item.checked ? checked_image : unchecked_image}
+                alt={item.checked ? 'Checked' : 'Unchecked'}
+              />
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export interface ItemListProps {
-	items: Item[]
-	setItems: React.Dispatch<React.SetStateAction<Item[] | undefined>>
+  items: Item[]
+  setItems: React.Dispatch<React.SetStateAction<Item[] | undefined>>
 }
 
 export type Item = {
-	checked: boolean
-	content: string
+  checked: boolean
+  content: string
 }
