2416c91d4b04e09836c23054a86044196eda0f7e
ZeSved <timothy.korell@gmail.com>
2023-10-06 22:39:48 +0200

    fixed input bug

--- src/components/ItemInput.tsx
+++ src/components/ItemInput.tsx
@@ -3,48 +3,52 @@ import styles from './styles/ItemInput.module.css'
 import { useState } from 'react'
 
 export default function ItemInput({ items, setItems }: ItemListProps) {
-  const [input, setInput] = useState('')
+	const [input, setInput] = useState('')
 
-  function addItem() {
-    setItems([
-      ...items,
-      {
-        content: `${input}`,
-        checked: false,
-      },
-    ])
+	function addItem() {
+		if (input.trim().length === 0) return
 
-    setInput('')
-  }
+		setItems([
+			...items,
+			{
+				content: `${input}`,
+				checked: false,
+			},
+		])
 
-  return (
-    <div className={styles.itemInputWrapper}>
-      <input
-        type="text"
-        placeholder="Type here to begin..."
-        value={input}
-        onChange={e => setInput(e.target.value)}
-        onKeyDown={e => {
-          if (e.key === 'Enter') {
-            e.preventDefault()
+		setInput('')
+	}
 
-            addItem()
-          }
-        }}
-      />
-      <button onClick={() => addItem()} className={styles.send}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="27"
-          viewBox="0 0 32 27"
-          fill="none">
-          <path
-            d="M0 26.6667V0L31.6667 13.3333L0 26.6667ZM3.33333 21.6667L23.0833 13.3333L3.33333 5V10.8333L13.3333 13.3333L3.33333 15.8333V21.6667Z"
-            fill="black"
-          />
-        </svg>
-      </button>
-    </div>
-  )
+	return (
+		<div className={styles.itemInputWrapper}>
+			<input
+				type='text'
+				placeholder='Type here to begin...'
+				value={input}
+				onChange={(e) => setInput(e.target.value)}
+				onKeyDown={(e) => {
+					if (e.key === 'Enter') {
+						e.preventDefault()
+
+						addItem()
+					}
+				}}
+			/>
+			<button
+				onClick={() => addItem()}
+				className={styles.send}>
+				<svg
+					xmlns='http://www.w3.org/2000/svg'
+					width='32'
+					height='27'
+					viewBox='0 0 32 27'
+					fill='none'>
+					<path
+						d='M0 26.6667V0L31.6667 13.3333L0 26.6667ZM3.33333 21.6667L23.0833 13.3333L3.33333 5V10.8333L13.3333 13.3333L3.33333 15.8333V21.6667Z'
+						fill='black'
+					/>
+				</svg>
+			</button>
+		</div>
+	)
 }
