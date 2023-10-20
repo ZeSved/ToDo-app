e0f9195047f6e439f050e3ce502172f22b7376b1
ZeSved <timothy.korell@gmail.com>
2023-10-20 22:08:15 +0200

    fixed build error

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -1,26 +1,26 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
 
-import { Item } from "../types"
+import { Item } from '../types'
 
-export default function ControlPanel({items}: ControlPanelType){
-    const [controlPanelInfo, setControlPanelInfo] = useState<number[]>([])
+export default function ControlPanel({ items }: ControlPanelType) {
+	const [controlPanelInfo, setControlPanelInfo] = useState<number[]>([])
 
-    useEffect(() => {
-        const tempArrFavorite: Item[] = items.filter(items => items.favorite === true)
-        const tempArrChecked: Item[] = items.filter(items => items.checked === true)
+	useEffect(() => {
+		// const tempArrFavorite: Item[] = items.filter(items => items.favorite === true)
+		// const tempArrChecked: Item[] = items.filter(items => items.checked === true)
 
-        setControlPanelInfo([0, 0])
-    }, [items])
+		setControlPanelInfo([0, 0])
+	}, [items])
 
-    return (
-        <div>
-            <p>Items: {items.length}</p>
-            <p>Favorites: {controlPanelInfo}</p>
-            <p>Checked: {controlPanelInfo}</p>
-        </div>
-    )
+	return (
+		<div>
+			<p>Items: {items.length}</p>
+			<p>Favorites: {controlPanelInfo}</p>
+			<p>Checked: {controlPanelInfo}</p>
+		</div>
+	)
 }
 
 type ControlPanelType = {
-    items: Item[]
+	items: Item[]
 }
