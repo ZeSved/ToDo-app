8aa6a83caaca3dc0b1fae51bb38a6bc6ec90c8f7
ZeSved <timothy.korell@gmail.com>
2023-10-20 22:36:15 +0200

    controlpanel visible

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 
+import styles from './styles/ControlPanel.module.css'
+
 import { Item } from '../types'
 
 export default function ControlPanel({ items }: ControlPanelType) {
@@ -13,10 +15,13 @@ export default function ControlPanel({ items }: ControlPanelType) {
 	}, [items])
 
 	return (
-		<div>
-			<p>Items: {items.length}</p>
-			<p>Favorites: {controlPanelInfo}</p>
-			<p>Checked: {controlPanelInfo}</p>
+		<div className={styles.controlPanelContainer}>
+			<div className={styles.info}>
+				<p>Items: {items.length}</p>
+				<p>Favorites: {controlPanelInfo[0]}</p>
+				<p>Checked: {controlPanelInfo[1]}</p>
+			</div>
+			<button className={styles.optionsDropdown}></button>
 		</div>
 	)
 }
