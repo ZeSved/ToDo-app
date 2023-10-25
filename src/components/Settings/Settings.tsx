a031c2c874a863b8aa9bf1d088584cb4f91ebab5
ZeSved <timothy.korell@gmail.com>
2023-10-25 17:52:31 +0200

    quick fix

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,41 +1,54 @@
-import { Custom } from "../../types"
+import { Custom } from '../../types'
+
 import styles from './Settings.module.css'
 
-export default function Settings({ DEFAULT_CUSTOM_SETTINGS }: SettingsProps){
-    let labelName: string
+export default function Settings({ DEFAULT_CUSTOM_SETTINGS }: SettingsProps) {
+	let labelName: string
 
-    function nameSwitch(settings: Custom){
-        switch (settings.name) {
-            case 'mainColor':
-                labelName = 'Main color: '
-                break;
-            case 'callToActionColor':
-                labelName = 'Secondary color: '
-                break;
-            case 'textColor':
-                labelName = 'Text color: '
-                break;
-        }
+	function nameSwitch(settings: Custom) {
+		switch (settings.name) {
+			case 'mainColor':
+				labelName = 'Main color: '
+				break
+			case 'callToActionColor':
+				labelName = 'Secondary color: '
+				break
+			case 'textColor':
+				labelName = 'Text color: '
+				break
+		}
 
-        return labelName
-    }
+		return labelName
+	}
 
-    return (
-        <>
-            <div className={styles.settingsContainer}>
-                {DEFAULT_CUSTOM_SETTINGS.map((settings) => (
-                    <div className={styles.inputContainer}>
-                        <label className={styles.inputLabel} htmlFor={settings.name}>{nameSwitch(settings)}</label>
-                        <input className={styles.settingsInput} name={settings.name} type="text" placeholder={settings.name} />
-                        <div className={styles.colorDisplay} style={{backgroundColor: settings.color}}/>
-                    </div>
-                ))}
-                <button className={styles.saveBtn}>Save</button>
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div className={styles.settingsContainer}>
+				{DEFAULT_CUSTOM_SETTINGS.map((settings) => (
+					<div className={styles.inputContainer}>
+						<label
+							className={styles.inputLabel}
+							htmlFor={settings.name}>
+							{nameSwitch(settings)}
+						</label>
+						<input
+							className={styles.settingsInput}
+							name={settings.name}
+							type='text'
+							placeholder={settings.name}
+						/>
+						<div
+							className={styles.colorDisplay}
+							style={{ backgroundColor: settings.color }}
+						/>
+					</div>
+				))}
+				<button className={styles.saveBtn}>Save</button>
+			</div>
+		</>
+	)
 }
 
 type SettingsProps = {
-    DEFAULT_CUSTOM_SETTINGS: Custom[]
+	DEFAULT_CUSTOM_SETTINGS: Custom[]
 }
